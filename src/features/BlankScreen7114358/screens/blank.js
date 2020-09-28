5327169f1e2dbc8829dea1294bb93074e606f251
Crowdbotics <team@crowdbotics.com>
2020-09-28 22:55:28 +0000

    Update components

--- src/features/BlankScreen7114358/screens/blank.js
+++ src/features/BlankScreen7114358/screens/blank.js
@@ -34,6 +34,57 @@ export class _Blank extends React.Component {
         }}
         style={this.props.themedStyle.Image_3}
       />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen6114357")}
+      >
+        <Button
+          textStyle={{
+            fontSize: 12,
+            color: "#000000",
+            textAlign: "center",
+            fontWeight: "normal",
+            fontStyle: "normal"
+          }}
+          style={this.props.themedStyle.Button_5}
+          onPress={() => alert("Pressed!")}
+        >
+          Client Info
+        </Button>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen8114359")}
+      >
+        <Button
+          textStyle={{
+            fontSize: 12,
+            color: "#000000",
+            textAlign: "center",
+            fontWeight: "normal",
+            fontStyle: "normal"
+          }}
+          style={this.props.themedStyle.Button_7}
+          onPress={() => alert("Pressed!")}
+        >
+          Project
+        </Button>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen0114261")}
+      >
+        <Button
+          textStyle={{
+            fontSize: 12,
+            color: "#000000",
+            textAlign: "center",
+            fontWeight: "normal",
+            fontStyle: "normal"
+          }}
+          style={this.props.themedStyle.Button_9}
+          onPress={() => alert("Pressed!")}
+        >
+          BaseOps Task
+        </Button>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -89,5 +140,98 @@ export default Blank = withStyles(_Blank, theme => ({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     borderRadius: 0
+  },
+  Button_5: {
+    width: "80%",
+    marginLeft: 50,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Button_7: {
+    width: "80%",
+    marginLeft: 50,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Button_9: {
+    width: "80%",
+    marginLeft: 50,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
