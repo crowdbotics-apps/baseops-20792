c18c0845619690790c9fba6c54796388ae432cf4
Crowdbotics <team@crowdbotics.com>
2020-09-28 20:42:12 +0000

    Update components

--- src/features/BlankScreen0114261/screens/blank.js
+++ src/features/BlankScreen0114261/screens/blank.js
@@ -34,6 +34,23 @@ export class _Blank extends React.Component {
         }}
         style={this.props.themedStyle.Image_3}
       />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen1114262")}
+      >
+        <Button
+          textStyle={{
+            fontSize: 12,
+            color: "#000000",
+            textAlign: "center",
+            fontWeight: "normal",
+            fontStyle: "normal"
+          }}
+          style={this.props.themedStyle.Button_5}
+          onPress={() => alert("Pressed!")}
+        >
+          Fuel
+        </Button>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -89,5 +106,37 @@ export default Blank = withStyles(_Blank, theme => ({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     borderRadius: 0
+  },
+  Button_5: {
+    width: "70%",
+    marginLeft: 35,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    alignSelf: "center",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#3366FF",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
